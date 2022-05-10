ac79e4078d61b3a1b4a0fe3c0984bebb5e079137
Ziqi Yu <88762490+Lucasyu777@users.noreply.github.com>
2022-05-11 02:12:26 +0800

    Add time picker with spinner

--- app/src/main/java/com/example/fit5046_lab5_groupe/fragment/DataEntryFragment.java
+++ app/src/main/java/com/example/fit5046_lab5_groupe/fragment/DataEntryFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.example.fit5046_lab5_groupe.databinding.DataEntryFragmentBinding;
 
@@ -22,12 +24,17 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
 public class DataEntryFragment extends Fragment {
     private DataEntryFragmentBinding dataBinding;
     private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
     private Button dateBtn;
+    private  Button timeBtn;
+
+    private int hour, minute;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,7 +57,35 @@ public class DataEntryFragment extends Fragment {
             }
         });
 
+
+        myTimePicker();
+        timeBtn = dataBinding.TimeBtn;
+        timeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePicker(view);
+            }
+        });
+
+
         return view;
+
+    }
+
+    private void myTimePicker() {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int sHour, int sMinute) {
+                hour = sHour;
+                minute = sMinute;
+                timeBtn.setText(String.format(Locale.getDefault(), "%02d:%02d",hour,minute));
+
+            }
+        };
+
+        int theme = AlertDialog.THEME_HOLO_LIGHT;
+        timePickerDialog = new TimePickerDialog(getActivity(),theme,onTimeSetListener, hour, minute,true);
+
 
     }
 
@@ -111,5 +146,9 @@ public class DataEntryFragment extends Fragment {
     public void showDatePicker(View view)
     {
         datePickerDialog.show();
+    }
+
+    public void showTimePicker(View view) {
+        timePickerDialog.show();
     }
 }
