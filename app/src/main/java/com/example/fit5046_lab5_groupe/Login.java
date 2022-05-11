edd11d4519133e1f196e3cc76f93ebf3aa09990d
Ziqi Yu <88762490+Lucasyu777@users.noreply.github.com>
2022-05-11 16:44:43 +0800

    Modify Login.java

--- app/src/main/java/com/example/fit5046_lab5_groupe/Login.java
+++ app/src/main/java/com/example/fit5046_lab5_groupe/Login.java
@@ -10,11 +10,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fit5046_lab5_groupe.databinding.ActivityLoginBinding;
+import com.example.fit5046_lab5_groupe.fragment.HomeFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     private ActivityLoginBinding binding;
+    private FirebaseAuth auth;
+
     private final ActivityResultLauncher<Intent> signUpLauncher =
             registerForActivityResult(
                     new ActivityResultContracts.StartActivityForResult(),
@@ -37,6 +45,9 @@ public class Login extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        auth = FirebaseAuth.getInstance();
+        EditText emailEditText = binding.emailAddressText;
+        EditText passwordEditText = binding.passwordText;
         binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,9 +58,26 @@ public class Login extends AppCompatActivity {
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                String txt_Email = emailEditText.getText().toString();
+                String txt_Pwd = passwordEditText.getText().toString();
+                loginUser(txt_Email,txt_Pwd);
             }
         });
+    }
+    private void loginUser(String txt_email, String txt_pwd) {
+        // call the object and provide it with email id and password
+        auth.signInWithEmailAndPassword(txt_email,
+                txt_pwd).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                String msg = "Login Successful";
+                toastMsg(msg);
+                startActivity(new Intent(Login.this,
+                        MainActivity.class));
+            }
+        });
+    }
+    public void toastMsg(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
